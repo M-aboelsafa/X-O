0476c403617975405827a30dd5ce59c15b0291b8
Mohamed youssef <78052557+M-aboelsafa@users.noreply.github.com>
2022-03-14 23:37:03 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,70 @@
+let title =document.querySelector('.title');
+let a =document.querySelector('.a');
+let turn = 0;
+let arr=[];
+let x,i;
+function end( num1, num2,num3)
+{
+
+a.innerHTML =' win ';
+document.getElementById(num1).style.background='#000';
+document.getElementById(num2).style.background='#000';
+document.getElementById(num3).style.background='#000';
+if(turn=1){
+    title.innerHTML='X';
+}
+else{
+    title.innerHTML='O';
+}
+turn = 4;
+setInterval(function(){a.innerHTML+='.'},1000);
+setTimeout(function(){location.reload()},4000)
+}
+function win()
+{
+    for (i=1;i<10;++i)
+    {
+        arr[i]= document.getElementById(i).innerHTML;
+    }
+    for(i=1;i<10;i+=3)
+    {
+    if(arr[i]==arr[i+1]&&arr[i]==arr[i+2]&&arr[i]!=='')
+    {
+        end(i,i+1,i+2);
+    }
+}
+for(i=1;i<4;i++)
+{
+if(arr[i]==arr[i+3]&&arr[i]==arr[i+6]&&arr[i]!=='')
+{
+    end(i,i+3,i+6);
+}
+}
+if(arr[1]==arr[5]&&arr[1]==arr[9]&&arr[9]!=='')
+{
+    end(1,5,9);
+}
+if(arr[3]==arr[5]&&arr[3]==arr[7]&&arr[7]!=='')
+{
+    end(3,5,7);
+}
+
+}
+function game(id)
+{
+    let element = document.getElementById(id);
+    if(turn == 0 && element.innerHTML=='')
+    {
+        element.innerHTML='X';
+        turn=1;
+        title.innerHTML='O';
+        win();
+    }
+    else if(turn == 1 && element.innerHTML=='')
+    {
+        turn=0;
+        element.innerHTML='O';
+        title.innerHTML='X';
+        win();
+    }
+} 
